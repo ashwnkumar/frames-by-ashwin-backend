1aa2bab22e7c90bbf48fdd51ca17b2ebda72c984
Ashwin Kumar <99787959+ashwnkumar@users.noreply.github.com>
2025-07-17 15:13:10 +0530

    Update envConfig.js

--- src/configs/envConfig.js
+++ src/configs/envConfig.js
@@ -6,6 +6,7 @@ const envConfig = {
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
   ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
+  FRONTEND_URL: process.env.FRONTEND_URL
 };
 
 module.exports = { envConfig };
