7df73cc2adb32d199813b973d290231026705c50
Ashwin Kumar <99787959+ashwnkumar@users.noreply.github.com>
2025-07-18 11:11:05 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,7 +3,7 @@ const { envConfig } = require("./envConfig")
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(`${envConfig.MONGO_URI}/frames-by-ashwn-backend`, {});
+        await mongoose.connect(`${envConfig.MONGO_URI}/frames-by-ashwin`, {});
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
